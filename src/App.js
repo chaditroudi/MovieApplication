5285b5658e4b7ed324dc99568f82917c6195124d
chaditroudi <troudishedy6@gmail.com>
2024-07-08 19:37:06 +0200

    finish the serach feature

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import MovieCard from "./components/MovieCard";
 import MovieList from "./components/MovieList";
 import { Header,Footer } from "./layout/index";
 import { Home } from "./pages/Home";
+import { useDispatch } from "react-redux";
+import { fetchAsyncMoviesList } from "./features/movies/moviesSlice";
 
 
 function App() {
+  const dispatch = useDispatch(); 
 
+
+  const handleSearch = (searchQuery) => {
+    dispatch(fetchAsyncMoviesList(searchQuery)); 
+  };
 
 return (
   <div className="app">
-  <Header/>
+  <Header onSearch={handleSearch}/>
 
   <div className="my-0 mx-10">
     <Routes>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import User from '../assets/images/user.png';
 
-import User from '../assets/images/user.png'
-export  const Header = () => {
+export const Header = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm);
+  };
 
   return (
     <div className="bg-white h-24 flex items-center justify-between py-0 px-10">
@@ -12,11 +22,14 @@ export  const Header = () => {
       <div className="w-6/12 flex justify-center">
         <form
           className="w-[70%] flex justify-center items-center"
+          onSubmit={handleSubmit}
         >
           <input
+            value={searchTerm}
+            onChange={handleInputChange}
             className="w-full text-lg px-[5px] pt-[5px] pb-[10px] h-10 outline-none"
             type="text"
-            placeholder="Search Movies or Shows"
+            placeholder="Search Movies"
           />
           <button
             className="py-0 px-2 m-2 flex justify-center items-center h-10 cursor-pointer text-xl bg-font-primary"
